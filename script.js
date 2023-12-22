91ec1f838929060ad21d5706ea219b922423dadd
Muhammadu Barry <muhammadubarry5@gmail.com>
2023-12-22 15:31:18 -0500

    updated file

--- script.js
+++ script.js
@@ -2,15 +2,11 @@ const addBookButton = document.querySelector("#add-book-button");
 const formContainer = document.querySelector(".form-container");
 
 // this is our form elements and values
-
 const form = document.createElement("form");
 const authorInput = document.createElement("input");
 const titleInput = document.createElement("input");
 const numberInput = document.createElement("input");
 const submitFormInput = document.createElement("input");
-const authorInputValue = authorInput.value;
-const titleInputValue = titleInput.value;
-const numberInputValue = numberInput.value;
 
 // this function lets our form to appear for our user input
 const getForm = () => {
@@ -42,20 +38,37 @@ const getForm = () => {
   formContainer.appendChild(form);
 };
 
-const inputValues = () => {
-  submitFormInput.addEventListener("click", () => {
-    console.log("form Received");
-    console.log(`author: ${authorInputValue}`);
-    console.log(`title: ${titleInputValue}`);
-    console.log(`Pages: ${numberInputValue}`);
-
-    const hideForm = form.style.display = "none";
-    return hideForm;
-  });
-};
-
-function addFormLimit(){
-    addBookButton.addEventListener('click', getForm);
-    inputValues();
+function addFormLimit() {
+  addBookButton.addEventListener("click", getForm);
 }
 addFormLimit();
+
+const myLibrary = [];
+
+const author = authorInput.value;
+const title = titleInput.value;
+const number = numberInput.value;
+
+class Book {
+  constructor(author, title, number) {
+    this.author = author;
+    this.title = title;
+    this.number = number;
+  }
+}
+class AddToLibrary extends Book {
+  constructor(author, title, number) {
+    super(author, title, number);
+    this.test = function () {
+      submitFormInput.addEventListener("click", function () {
+        console.log(`author: ${author}`);
+        console.log(`title: ${title}`);
+        console.log(`pages: ${number}`);
+        console.log("book submitted Thank you");
+      });
+    };
+  }
+}
+
+const harryPotter = new AddToLibrary();
+harryPotter.test();

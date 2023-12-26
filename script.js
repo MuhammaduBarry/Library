636a36f30037b32ae75abb6925d75b2b8d9803b7
Muhammadu Barry <muhammadubarry5@gmail.com>
2023-12-26 16:16:42 -0500

    updated files

--- script.js
+++ script.js
@@ -5,49 +5,30 @@ const body = document.querySelector("body");
 
 // Creating form elements
 const form = document.createElement("form");
+const headerForm = document.createElement("h3");
 const authorInput = document.createElement("input");
 const titleInput = document.createElement("input");
 const numberInput = document.createElement("input");
 const submitFormInput = document.createElement("input");
-
-// our overlay form
-const overlay = () => {
-  // Define isOverlay variable
-  let isOverlay = false;
-
-  // Function to toggle overlay class
-  const toggleOverlay = () => {
-    if (isOverlay) {
-      body.classList.remove("overlay");
-    } else {
-      body.classList.add("overlay");
-    }
-
-    // Update the isOverlay variable after toggling the class
-    isOverlay = !isOverlay;
-  };
-
-  // Event listener for addBookButton
-  addBookButton.addEventListener("click", toggleOverlay);
-
-  submitFormInput.addEventListener("click", toggleOverlay);
-};
+const iTag = document.createElement("i");
 
 // Function to create the form
 const getForm = () => {
+  iTag.classList.add("bi", "bi-x", "x-css-style");
+
+  headerForm.innerText = "Add New Book";
+  headerForm.classList.add("header-form-style");
   // Configuring author input
   authorInput.type = "text";
   authorInput.placeholder = "Author";
   authorInput.id = "author-input";
   authorInput.autocomplete = "off";
-  form.appendChild(authorInput);
 
   // Configuring title input
   titleInput.type = "text";
   titleInput.placeholder = "Title";
   titleInput.id = "title-input";
   titleInput.autocomplete = "off";
-  form.appendChild(titleInput);
 
   // Configuring number input
   numberInput.type = "number";
@@ -55,18 +36,28 @@ const getForm = () => {
   numberInput.id = "number-input";
   numberInput.min = 0;
   numberInput.inputMode = "numeric";
-  form.appendChild(numberInput);
 
   // Configuring submit button
   submitFormInput.type = "submit";
   submitFormInput.value = "submit";
   submitFormInput.id = "submit-form-input";
-  form.appendChild(submitFormInput);
+
+  // Configuring form animation
+  form.classList.add("animate__animated", "animate__pulse");
 
   // Appending the form to the form container
+  form.appendChild(iTag);
+  form.appendChild(headerForm);
+  form.appendChild(authorInput);
+  form.appendChild(titleInput);
+  form.appendChild(numberInput);
+  form.appendChild(submitFormInput);
   formContainer.appendChild(form);
-};
 
+  body.classList.add("overlay");
+
+  console.log("opening form");
+};
 // Class constructor for Book
 class Book {
   constructor(author, title, pages) {
@@ -81,77 +72,92 @@ let myLibrary = [];
 
 // Function to create a new book card and add it to the library
 const addToLibrary = () => {
-  // Create a new book object
-  const newBook = new Book(
-    authorInput.value,
-    titleInput.value,
-    numberInput.value
-  );
+  // Set required attribute to true
+  authorInput.required = true;
+  titleInput.required = true;
+  numberInput.required = true;
 
-  // Add the new book to the library array
-  myLibrary.push(newBook);
+  // checks if it's true if not it will report it down below
+  if (form.checkValidity()) {
+    // Create a new book object
+    const newBook = new Book(
+      authorInput.value,
+      titleInput.value,
+      numberInput.value
+    );
 
-  // Clear existing book cards
-  mainBookContainer.innerHTML = "";
+    // Add the new book to the library array
+    myLibrary.push(newBook);
 
-  // Loop through the library array and display each book
-  // book in this case is our newBook
-  myLibrary.forEach((book) => {
-    // Create new elements for each book
-    const newBookCardContainer = document.createElement("div");
-    newBookCardContainer.classList.add("card-container");
+    // Clear existing book cards
+    mainBookContainer.innerHTML = "";
 
-    const newAuthorTag = document.createElement("h3");
-    newAuthorTag.classList.add("card-author-display");
-    newAuthorTag.innerText = `Author: ${book.author}`;
+    authorInput.setAttribute("required", "true");
+    titleInput.setAttribute("required", "true");
+    numberInput.setAttribute("required", "true");
 
-    const newTitleTag = document.createElement("h3");
-    newTitleTag.classList.add("card-title-display");
-    newTitleTag.innerText = `Title: ${book.title}`;
+    // Loop through the library array and display each book
+    // book in this case is our newBook
+    myLibrary.forEach((book) => {
+      // Create new elements for each book
+      const newBookCardContainer = document.createElement("div");
+      newBookCardContainer.classList.add("card-container");
 
-    const newNumberTag = document.createElement("h3");
-    newNumberTag.classList.add("card-number-display");
-    newNumberTag.innerText = `Pages: ${book.pages}`;
+      const newAuthorTag = document.createElement("h3");
+      newAuthorTag.classList.add("card-author-display");
+      newAuthorTag.innerText = `Author: ${book.author}`;
 
-    const newSliderContainer = document.createElement("div");
-    newSliderContainer.classList.add("slider-container");
+      const newTitleTag = document.createElement("h3");
+      newTitleTag.classList.add("card-title-display");
+      newTitleTag.innerText = `Title: ${book.title}`;
 
-    const newReadToggle = document.createElement("button");
-    newReadToggle.innerText = "READING";
-    newReadToggle.classList.add("read-button");
+      const newNumberTag = document.createElement("h3");
+      newNumberTag.classList.add("card-number-display");
+      newNumberTag.innerText = `Pages: ${book.pages}`;
 
-    let isReading = true;
+      const newSliderContainer = document.createElement("div");
+      newSliderContainer.classList.add("slider-container");
 
-    newReadToggle.addEventListener("click", () => {
-      if (isReading) {
-        newReadToggle.style.backgroundColor = "red";
-        newReadToggle.innerText = "FINISHED";
-      } else {
-        newReadToggle.style.backgroundColor = "";
-        newReadToggle.innerText = "READING";
-      }
+      const newReadToggle = document.createElement("button");
+      newReadToggle.innerText = "READING";
+      newReadToggle.classList.add("read-button");
 
-      isReading = !isReading;
+      let isReading = true;
+
+      newReadToggle.addEventListener("click", () => {
+        if (isReading) {
+          newReadToggle.style.backgroundColor = "red";
+          newReadToggle.innerText = "FINISHED";
+        } else {
+          newReadToggle.style.backgroundColor = "";
+          newReadToggle.innerText = "READING";
+        }
+
+        isReading = !isReading;
+      });
+
+      // Append elements to the new card container
+      newBookCardContainer.appendChild(newAuthorTag);
+      newBookCardContainer.appendChild(newTitleTag);
+      newBookCardContainer.appendChild(newNumberTag);
+
+      newSliderContainer.appendChild(newReadToggle);
+      newBookCardContainer.appendChild(newSliderContainer);
+
+      mainBookContainer.appendChild(newBookCardContainer);
     });
 
-    // Append elements to the new card container
-    newBookCardContainer.appendChild(newAuthorTag);
-    newBookCardContainer.appendChild(newTitleTag);
-    newBookCardContainer.appendChild(newNumberTag);
+    body.classList.remove("overlay");
 
-    newSliderContainer.appendChild(newReadToggle);
-    newBookCardContainer.appendChild(newSliderContainer);
-
-    mainBookContainer.appendChild(newBookCardContainer);
-  });
-
-  // Reset form values
-  authorInput.value = "";
-  titleInput.value = "";
-  numberInput.value = "";
-
-  // Remove the form
-  formContainer.removeChild(form);
+    // Reset form values
+    authorInput.value = "";
+    titleInput.value = "";
+    numberInput.value = "";
+    // Remove the form
+    formContainer.removeChild(form);
+  } else {
+    form.reportValidity();
+  }
 };
 
 // Event listeners for form and submit button
@@ -162,6 +168,7 @@ const addFormAndRemove = () => {
   // Event listener for form submission
   submitFormInput.addEventListener("click", (e) => {
     e.preventDefault();
+
     // Call addToLibrary when the form is submitted
     addToLibrary();
   });
@@ -174,9 +181,11 @@ const addFormAndRemove = () => {
   });
 };
 
+iTag.addEventListener("click", () => {
+  formContainer.removeChild(form);
+  body.classList.remove("overlay");
+});
+
+
 // Initialize the form and event listeners
 addFormAndRemove();
-
-document.addEventListener("DOMContentLoaded", overlay);
-
-// just testing
